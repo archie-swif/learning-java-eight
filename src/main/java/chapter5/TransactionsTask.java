19b7cf48661e851d1935e071f919a4226fd32346
archie-swif <artem.ryabokon@gmail.com>
2016-03-24 19:47:11 +0200

    Added task from java 8 in action

--- src/main/java/chapter5/TransactionsTask.java
+++ src/main/java/chapter5/TransactionsTask.java
@@ -3,6 +3,8 @@ package chapter5;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.junit.Assert;
@@ -20,18 +22,46 @@ public class TransactionsTask {
 					mario, 2012, 710), new Transaction(mario, 2012, 700), new Transaction(alan,
 					2012, 950));
 
-	/**
-	 * Find all transactions in the year 2011 and sort them by value (small to
-	 * high).
-	 */
+	// 1. Find all transactions in the year 2011 and sort them by value (small
+	// to high).
+	// 2. What are all the unique cities where the traders work?
+	// 3. Find all traders from Cambridge and sort them by name.
+	// 4. Return a string of all traders’ names sorted alphabetically.
+	// 5. Are any traders based in Milan?
+	// 6. Print all transactions’ values from the traders living in Cambridge.
+	// 7. What’s the highest value of all the transactions?
+	// 8. Find the transaction with the smallest value.
+	// 9. Make a (tr. value -> transaction) map from a list
+
 	@Test
 	public void testOne() {
 
-		List<Transaction> filtered = transactions.stream().filter(t -> t.getYear() == 2011)
+		List<Transaction> result = transactions.stream().filter(t -> t.getYear() == 2011)
 				.sorted(Comparator.comparing(Transaction::getValue)).collect(Collectors.toList());
 
-		System.out.println(filtered);
-		Assert.assertEquals(2, filtered.size());
+		System.out.println(result);
+		Assert.assertEquals(2, result.size());
+
+	}
+
+	@Test
+	public void testTwo() {
+
+		List<String> result = transactions.stream().map(t -> t.getTrader().getName()).distinct()
+				.collect(Collectors.toList());
+
+		System.out.println(result);
+		Assert.assertEquals(4, result.size());
+
+	}
+
+	@Test
+	public void testListToMap() {
+
+		Map<Integer, Transaction> result = transactions.stream().collect(
+				Collectors.toMap(Transaction::getValue, Function.identity()));
+
+		System.out.println(result);
 
 	}
 }
